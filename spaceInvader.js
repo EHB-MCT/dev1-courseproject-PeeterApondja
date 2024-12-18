fd8e6406683e35f77eba609cb28a8da152c7ab45
Peeter Apondja <peeter.apondja@student.ehb.be>
2024-12-18 23:24:35 +0100

    versie 1

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,32 @@
-"use strict";
+// Haal het canvas-element en de tekencontext op
+const canvas = document.getElementById("avatarCanvas");
+const ctx = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+// Definieer de grootte van elk blok en beginposities
+const blockSize = 50; // Grootte van de blokken
+
+// Vast patroon voor het avatarontwerp
+const fixedPattern = [
+	[1, 1, 0, 0, 0, 1, 1],
+	[1, 0, 1, 0, 1, 0, 1],
+	[1, 0, 0, 1, 0, 0, 1],
+	[1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 0, 1, 0, 0, 1],
+	[1, 0, 1, 0, 1, 0, 1],
+	[1, 1, 0, 0, 0, 1, 1],
+];
+
+// Teken het vaste patroon op het canvas
+function drawAvatar() {
+	for (let row = 0; row < fixedPattern.length; row++) {
+		for (let col = 0; col < fixedPattern[row].length; col++) {
+			if (fixedPattern[row][col] === 1) {
+				ctx.fillStyle = "limegreen";
+				ctx.fillRect(col * blockSize, row * blockSize, blockSize, blockSize);
+			}
+		}
+	}
+}
+
+// Roep de functie aan om het avatar te tekenen
+drawAvatar();
